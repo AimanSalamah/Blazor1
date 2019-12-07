17aefcae02f3b58e798b6dfbeb340870715391fc
Aiman Salamah <aiman@root.sa>
2019-12-07 22:19:01 +0300

    fix message modal to use data model

---
+++ mkabbani/Data/ModalMessage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mkabbani.Data
+{
+    public class ModalMessage
+    {
+        public string Display { get; set; }
+        public string MessageText { get; set; }
+        public string MessageTitle { get; set; }
+        private bool _ShowAlert;
+        public bool ShowAlert
+        {
+            get { return this._ShowAlert; }
+            set
+            {
+
+                this._ShowAlert = value;
+                if (value) this.Display = "block"; else this.Display = "none";
+            }
+        }
+        public ModalMessage()
+        {
+            this.Display = "none";
+        }
+        public void Show(string message, string title= "")
+        {
+            this.MessageText = message;
+            this.MessageTitle = title;
+            this.ShowAlert = true;
+        }
+        public void Hide()
+        {
+            this.MessageText = string.Empty;
+            this.MessageTitle = string.Empty;
+            this.ShowAlert = false;
+        }
+    }
+}
